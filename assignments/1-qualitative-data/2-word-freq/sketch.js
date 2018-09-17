96a40407243ee3b4e192f6dcc7bd157b5b38e46d
andrew_l <andrewlevins@gmail.com>
2018-09-17 15:11:31 -0400

    progress on word frequency assignment 1

--- assignments/1-qualitative-data/2-word-freq/sketch.js
+++ assignments/1-qualitative-data/2-word-freq/sketch.js
@@ -4,89 +4,106 @@ let growMax;
 let myText;
 let canvasX = innerWidth * 0.8;
 let canvasY = innerHeight * 0.6;
-let filters = {};
+
+// the value of each key is an array with the values to filter
+let filters = {
+  word: [
+    "enterprise",
+    "enterprises",
+    "business",
+    "work",
+    "small business",
+    "training",
+    "entrepreneurs",
+    "jobs",
+    "economic",
+    "growth",
+    "cost",
+    "economies",
+    "model",
+    "models",
+    "prive",
+    "sector",
+    "create",
+    "financing",
+    "viable",
+    "subsidies",
+    "incentivize",
+    "pricing",
+    "entrepreneurial",
+    "expansion",
+    "chad"
+  ]
+};
+
+function addWord() {
+  let a = document.getElementById("add").value;
+  let b = a.toLowerCase();
+  const content = document.querySelector(".warning");
+  content.classList.add("active");
+  if (filters.word.includes(b)) {
+    console.log(b, "already exists");
+    content.textContent = b + " already exists!";
+  } else {
+    filters.word.push(b);
+    content.textContent = "Added: " + "'" + b + "'";
+    setup();
+  }
+}
 
 function setup() {
   canvas = createCanvas(canvasX, canvasY);
   canvas.parent("sketch-holder");
   background("#3d3d3d");
-  myText = new Text("africa.txt", 50);
+  textFont("monospace");
+  // title
+  fill("#fff");
+  textSize(22);
+  textAlign(CENTER);
+  text("SDG 8: DECENT WORK AND ECONOMIC GROWTH", width / 2, height / 2);
+  textSize(16);
+  text("Scroll down or drag to visualize word counts", width / 2, height / 1.8);
+  myText = new Text("africa.txt", 10);
 }
 
 // scrolling option
 function draw() {
   myText.display();
 }
-// function addWord() {
-//   var a = document.getElementById("add").value;
-//   filters.word = a;
-// }
+
 function Text(fileName, xPos) {
   const dictionary = [];
   loadStrings(fileName, callback);
 
   this.display = function() {
-    // the value of each key is an array with the values to filter
-    filters = {
-      word: [
-        "africa",
-        "enterprise",
-        "enterprises",
-        "business",
-        "work",
-        "small business",
-        "training",
-        "entrepreneurs",
-        "jobs",
-        "economic",
-        "growth",
-        "cost",
-        "economies",
-        "model",
-        "models",
-        "prive",
-        "sector",
-        "create",
-        "financing",
-        "viable",
-        "subsidies",
-        "incentivize",
-        "pricing",
-        "entrepreneurial",
-        "expansion",
-        "chad"
-      ]
-    };
-
     // see multiFilter.js file for multi-criteria filtering function
-    var n = multiFilter(dictionary, filters);
-
+    var x = multiFilter(dictionary, filters);
+    // narrow down to top 20 words
+    var n = x.slice(0, 20);
     push();
+    textSize(16);
+    textAlign(LEFT);
     translate(xPos, 0);
 
     for (var i = 0; i < n.length; i++) {
-      // let n = dictionary[i].word.includes("africa");
-
       let displayCount = n[i].count;
       let growing = growBar * 0.1 * displayCount;
-      var xc = constrain(growing, 50, innerWidth * 0.75);
+      var xc = constrain(growing, 10, innerWidth * 0.78);
 
-      // let txtWidth = textWidth(dictionary[i].word + displayCount + 0);
-      fill(255 - i * 10);
-      // rect(25, 25 + i * 20, txtWidth, 16);
+      fill(255 - i * 7);
+      stroke("#3d3d3d");
+      rect(0, 10, xc, 20, 10);
+      fill(0);
       noStroke();
-      rect(0, 10, xc, 20);
-      // textSize(dictionary[i].count * 1.5);
-      fill(0 + i * 50);
 
-      text(n[i].word + " " + displayCount, 10, 26);
-      translate(0, 21);
+      fill("#3d3d3d");
+      textAlign(LEFT);
+      text(n[i].word, 10, 26);
+      textAlign(RIGHT);
+      text(displayCount + "x", xc - 10, 26);
+      translate(0, 20);
     }
     pop();
-
-    fill("#3d3d3d");
-
-    text("SDG", width / 2, height / 2);
   };
 
   // dictionary
@@ -95,8 +112,10 @@ function Text(fileName, xPos) {
       var words = phrases.split(" ");
       words.forEach(function(word) {
         word = word.toLowerCase();
-
-        let filteredWords = dictionary.filter(el => {
+        let removeBlanks = dictionary.filter(el => {
+          return el.word !== "";
+        });
+        let filteredWords = removeBlanks.filter(el => {
           return el.word == word;
         });
         if (filteredWords.length) {
@@ -117,18 +136,28 @@ function Text(fileName, xPos) {
 // dragging option
 function mouseDragged() {
   background("#3d3d3d");
+  // title
+  fill("#fff");
+  textSize(22);
+  textAlign(CENTER);
+  text("SDG 8: DECENT WORK AND ECONOMIC GROWTH", width / 2, height / 2);
+  textSize(16);
+  text("Scroll down or drag to visualize word counts", width / 2, height / 1.8);
 
-  // myText.display();
-
-  textY += mouseY - pmouseY;
+  growBar += mouseX - pmouseX;
+  growBar += mouseY - pmouseY;
 }
 
 // scrolling option
 function mouseWheel(event) {
   background("#3d3d3d");
+  // title
+  fill("#fff");
+  textSize(22);
+  textAlign(CENTER);
+  text("SDG 8: DECENT WORK AND ECONOMIC GROWTH", width / 2, height / 2);
+  textSize(16);
+  text("Scroll down or drag to visualize word counts", width / 2, height / 1.8);
 
-  // myText.display();
-
-  // textY += -event.delta;
   growBar -= -event.delta;
 }
