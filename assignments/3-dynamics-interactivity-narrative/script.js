720c6ff0b6d591f8dda76b05a648f51163600af9
andrew_l <andrewlevins@gmail.com>
2018-11-12 14:36:40 -0500

    progress on assignment 3 section 1

--- assignments/3-dynamics-interactivity-narrative/script.js
+++ assignments/3-dynamics-interactivity-narrative/script.js
@@ -49,63 +49,214 @@ if (window.innerWidth < 925) {
 }
 
 let svg = (data, firstGraph, secondGraph) => {
-  var svg1 = d3
-    .select(firstGraph)
-    .append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .attr("fill", "#3d3d3d");
+  // first chart
+  const chartOne = d3.csv("data/cleaned/chartone.csv", d => {
+    return {
+      tier: d["tier"],
+      income: +d["income"],
+      unemployment: +d["unemployment"],
+      life: +d["life"]
+    };
+  });
+  chartOne.then(d => {
+    var svg1 = d3
+      .select(firstGraph)
+      .append("svg")
+      .attr("width", width)
+      .attr("height", height);
 
-  let group1 = svg1.append("g").attr("id", "group");
+    let group1 = svg1.append("g").attr("id", "group");
 
-  let bars = group1
-    .selectAll("g")
-    .data(data)
-    .enter()
-    .append("g")
-    .attr("transform", (d, i) => {
-      return `translate(0, ${(i * height) / data.length})`;
-    });
+    let bars = group1
+      .selectAll("g")
+      .data(d)
+      .enter()
+      .append("g")
+      .attr("transform", (d, i) => {
+        return `translate(0, ${(i * height) / data.length})`;
+      })
+      .classed("top-green", d => {
+        return d.tier == "top";
+      })
+      .classed("bottom-red", d => {
+        return d.tier == "bottom";
+      });
 
-  // visual bar
-  bars
-    .append("rect")
-    .attr("height", "10px") // TODO
-    .attr("width", (d, i) => {
-      return d.cost;
-    }) // TODO
-    .attr("y", (d, i) => {
-      return i * 2;
-    })
-    .attr("x", (d, i) => {
-      return width - d.cost;
-    })
-    .attr("id", "output");
+    // axis title
+    svg1
+      .append("text")
+      .attr("text-anchor", "end")
+      .text("Ranking")
+      .attr("x", width * 0.2)
+      .attr("y", height / 4);
 
-  // var colors = ["orange", "purple", "steelblue", "pink", "blue", "green"];
-  // var r = [0, 40, 30, 25, 80, 130];
-  // var circle = svg1
-  //   .append("circle")
-  //   .attr("cx", width / 2)
-  //   .attr("cy", height / 2)
-  //   .attr("r", r[0]);
+    // text labels on points
+    bars
+      .append("text")
+      .attr("text-anchor", "end")
+      .attr("x", width * 0.2)
+      .attr("y", (d, i) => {
+        return height / 3 + i * 100 + 8;
+      })
+      .text(d => {
+        return `${d.tier} 10%`;
+      });
 
-  d3.graphScroll()
-    .container(d3.select(".container-1"))
-    .graph(d3.selectAll("container-1 #graph"))
-    .eventId("uniqueId1") // namespace for scroll and resize events
-    .sections(d3.selectAll(".container-1 #sections > div"))
-    // .offset(innerWidth < 900 ? innerHeight - 30 : 200)
-    .on("active", function(i) {
-      // circle
-      //   .transition()
-      //   .duration(1000)
-      //   .attr("r", r[i])
-      //   .transition()
-      //   .style("fill", colors[i]);
-      console.log(i);
-    });
+    d3.graphScroll()
+      .container(d3.select(".container-1"))
+      .graph(d3.selectAll("container-1 #graph"))
+      .eventId("uniqueId1") // namespace for scroll and resize events
+      .sections(d3.selectAll(".container-1 #sections > div"))
+      // .offset(innerWidth < 900 ? innerHeight - 30 : 200)
+      .on("active", function(i) {
+        console.log(i);
+        if (i == 1) {
+          svg1.selectAll("#titled").remove();
+          bars.selectAll("#label").remove();
+          bars.selectAll("rect").remove();
+          // number format
+          let format = d3.format(",d");
 
+          // update title
+          svg1
+            .append("text")
+            .text("Adjusted Net National Income (Current US$)")
+            .attr("text-anchor", "middle")
+            .attr("x", width / 2)
+            .attr("y", 50)
+            .attr("id", "titled");
+
+          //update bars
+          bars
+            .attr("id", "output")
+            .append("rect")
+            .attr("height", "10px")
+            .attr("y", (d, i) => {
+              return height / 3 + i * 100;
+            })
+            .attr("x", width * 0.2 + 10)
+            .transition()
+            .duration(1000)
+            .attr("width", (d, i) => {
+              return d.income * 0.013;
+            });
+
+          // text labels on bars
+          bars
+            .append("text")
+            .attr("id", "label")
+            .attr("text-anchor", "start")
+            .attr("x", (d, i) => {
+              return d.income * 0.013 + (width * 0.2 + 12);
+            })
+            .attr("y", (d, i) => {
+              return height / 3 + i * 100 + 8;
+            })
+            .transition()
+            .delay(1000)
+            .text(d => {
+              return `$${format(d.income)} per capita`;
+            });
+        } else if (i == 2) {
+          svg1.selectAll("#titled").remove();
+          bars.selectAll("#label").remove();
+          bars.selectAll("rect").remove();
+          // number format
+          let format = d3.format(".2f");
+
+          // update title
+          svg1
+            .append("text")
+            .text("Unemployment %")
+            .attr("text-anchor", "middle")
+            .attr("x", width / 2)
+            .attr("y", 50)
+            .attr("id", "titled");
+
+          //update bars
+          bars
+            .attr("id", "output")
+            .append("rect")
+            .attr("height", "10px")
+            .attr("y", (d, i) => {
+              return height / 3 + i * 100;
+            })
+            .attr("x", width * 0.2 + 10)
+            .transition()
+            .duration(1000)
+            .attr("width", (d, i) => {
+              return d.unemployment * 20;
+            });
+
+          // text labels on bars
+          bars
+            .append("text")
+            .attr("id", "label")
+            .attr("text-anchor", "start")
+            .attr("x", (d, i) => {
+              return d.unemployment * 20 + (width * 0.2 + 12);
+            })
+            .attr("y", (d, i) => {
+              return height / 3 + i * 100 + 8;
+            })
+            .transition()
+            .delay(1000)
+            .text(d => {
+              return `${format(d.unemployment)}%`;
+            });
+        } else if (i == 3) {
+          svg1.selectAll("#titled").remove();
+          bars.selectAll("#label").remove();
+          bars.selectAll("rect").remove();
+
+          // number format
+          let format = d3.format(".2f");
+
+          // update title
+          svg1
+            .append("text")
+            .text("Life Expectancy")
+            .attr("text-anchor", "middle")
+            .attr("x", width / 2)
+            .attr("y", 50)
+            .attr("id", "titled");
+
+          //update bars
+          bars
+            .attr("id", "output")
+            .append("rect")
+            .attr("height", "10px")
+            .attr("y", (d, i) => {
+              return height / 3 + i * 100;
+            })
+            .attr("x", width * 0.2 + 10)
+            .transition()
+            .duration(1000)
+            .attr("width", (d, i) => {
+              return d.life * 5;
+            });
+
+          // text labels on bars
+          bars
+            .append("text")
+            .attr("id", "label")
+            .attr("text-anchor", "start")
+            .attr("x", (d, i) => {
+              return d.life * 5 + (width * 0.2 + 12);
+            })
+            .attr("y", (d, i) => {
+              return height / 3 + i * 100 + 8;
+            })
+            .transition()
+            .delay(1000)
+            .text(d => {
+              return `${format(d.life)} years`;
+            });
+        }
+      });
+  });
+
+  // second chart
   // scatter plot
   // linear scale x
   let x = d3
